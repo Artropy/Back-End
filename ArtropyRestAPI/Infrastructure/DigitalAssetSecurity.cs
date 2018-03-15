8e8591a73c6838c6c99cd6307f518496e7ea2cf7
Stephen Moreira <smoreira1@live.com>
2018-03-14 21:59:06 -0400

    commit all

---
+++ ArtropyRestAPI/Infrastructure/DigitalAssetSecurity.cs
@@ -0,0 +1,26 @@
+using ArtropyRestApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtropyRestApi.Infrastructure
+{
+    public class DigitalAssetSecurity
+    {
+
+          public DigitalAsset DigitalAsset { get; set;}
+
+          public void checkForPlagiarism()
+          {
+
+          }
+
+          public void copyRightDigitalAsset()
+          {
+
+          }
+
+
+    }
+}
---
+++ ArtropyRestAPI/Model/Sponser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtropyRestApi.Model
+{
+    public class Sponser
+    {
+        public int id { get; set; }
+        public string sponserName { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
